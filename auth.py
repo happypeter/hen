1e18a6a0b86481f8247c5cf6e5de9635b7665e5a
Peter King <happypeter1983@gmail.com>
2009-10-10 08:38:36 +0800

    works

--- auth.py
+++ auth.py
@@ -1,7 +1,9 @@
+import os
 def get_ak():
 	f=open("/var/axtu/asianux-auth","rw");
 	print f.readline()
-
+def exe_auth_client():
+	os.system("peter-hello")
 from yum.plugins import PluginYumExit, TYPE_CORE, TYPE_INTERACTIVE
 
 requires_api_version = '2.3'
@@ -10,6 +12,7 @@ plugin_type = (TYPE_CORE, TYPE_INTERACTIVE)
 def init_hook(conduit):
 	conduit.info(2, 'Hello world')
 	get_ak()
+	exe_auth_client()
 def postreposetup_hook(conduit):
 	raise PluginYumExit('Goodbye')
 
