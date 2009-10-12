b24f76138ad2c09a7a1a9d5eb1c962370062f7bc
Peter King <happypeter1983@gmail.com>
2009-10-12 16:34:09 +0800

    for test

--- html-creator/kkk.py
+++ html-creator/kkk.py
@@ -2,13 +2,13 @@
 # 1. read all the file name in a dir
 
 import os
-import sys
 ooo=os.listdir('./')
 #everything is read into list ooo
 ooo.sort()#sorted items are easier to locate
 os.makedirs("peter-html")#create dir to store all the output
 os.makedirs("peter-html/html")
 f=open("./peter-html/index.html","w");
+f.write("<h1>Index</h1>")
 for s in ooo:
     os.system("cp " + s + " " + s + ".html")
     f.write('<a href="./html/')
@@ -16,6 +16,8 @@ for s in ooo:
     f.write('">')
     f.write(s)
     f.write('</a>---<br>\n')
+    k=open(s,"w")
+    k.wirte("<pre>")
 
 os.system("mv  *.html ./peter-html/html/")
 
