ad52bdaae8bb88f346a2a1b882c56415f5d23087
Peter King <happypeter1983@gmail.com>
2009-10-12 14:47:26 +0800

    getopt has some problem

--- getopt.py
+++ getopt.py
@@ -1,15 +1,11 @@
-#http://www.faqs.org/docs/diveintopython/kgp_commandline.html
-import sys
 import getopt
-def usage():
-	print "I am usage()`"
+import sys
 def main(argv):                         
-    grammar = "kant.xml"                 
+    grammar = "kant.xml"    
     try:                                
-        opts, args = getopt.getopt(argv, "hg:k:pd", ["help", "grammar="]) 
-    except getopt.GetoptError:           
-        usage()                          
-        sys.exit(2)                     
-
+        opts, args = getopt.getopt(argv, "hg:d", ["help", "grammar="]) 
+    except getopt.GetoptError:     
+        usage()           
+        sys.exit(2)     
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1:])  
