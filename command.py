6b6f8d6b8c68a034cfb245b8c7bd3422b7f8a27a
Peter King <happypeter1983@gmail.com>
2009-10-12 10:20:47 +0800

    nice

--- command.py
+++ command.py
@@ -2,7 +2,10 @@
 #this file should be merged into auth.py later
 #argecho.py
 import sys
-
+import os
+def exe_auth_client():
+        os.system("ls")
 for arg in sys.argv:
 	print arg
+	exe_auth_client()
 
