c6b6bee5e67fad66e825edc6b468a1582cfff7b1
Peter King <happypeter1983@gmail.com>
2009-10-14 10:32:06 +0800

    s

--- sock.py
+++ sock.py
@@ -1,5 +1,7 @@
 from socket import *
 sock = socket(AF_INET, SOCK_STREAM)
-sock.connect(('mail.rmi.net',110))#110--- port num for POP mail server
+sock.connect(('starship.python.net',80))#110--- port num for POP mail server
+sock.send('GET /\r\n')
+
 print sock.recv(60)
 
