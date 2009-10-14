df3297efee84d82a119f5a0ef482efaf00392b1c
Peter King <happypeter1983@gmail.com>
2009-10-14 10:36:42 +0800

    s

--- sock.py
+++ sock.py
@@ -3,5 +3,9 @@ sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(('starship.python.net',80))#110--- port num for POP mail server
 sock.send('GET /\r\n')
 
-print sock.recv(60)
+print sock.recv(1000)
 
+#now I have all the html tags and everthing, there are a lot I can do
+#e.g I can define my own tag <peter-data></peter-data>
+#then when I get the tag on my client, I can code to do anything to it
+#before send the page to firefox
