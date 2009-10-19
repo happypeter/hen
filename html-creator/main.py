8b33fe43e6dc192dce3ea105bad881760d194f4e
Peter King <happypeter1983@gmail.com>
2009-10-19 18:01:17 +0800

    s

--- html-creator/main.py
+++ html-creator/main.py
@@ -7,43 +7,24 @@
 # 2. and all the other good tags for general use
 # 3. read out the first line of each file as its desc in index.html
 import os
-
-
-def ensure_dir(f):
-    d = os.path.dirname(f)
-    if not os.path.exists(d):
-        os.makedirs(d)
+html_tab="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
 dir=os.popen("python ./list.py").read()
-print dir
+dir = dir.replace('[','')
+dir = dir.replace(']','')
 listtt=dir.split('\n')
-print "ppppppppppp"
 print listtt
 
-ooo=os.listdir('./')
-#everything is read into list ooo
-ooo.sort()#sorted items are easier to locate
-
-#create dir to store all the output
-#ensure_dir("./peter-html")
-#ensure_dir("./peter-html/html")
-os.makedirs("./peter-html")
-os.makedirs("./peter-html/html")
-   
-f=open("./peter-html/index.html","w");
+f=open("index.html","w");
 f.write("<h1>Index</h1>")
-for s in ooo:
-    os.system("cp "+s+" "+s+".txt")
-#we use .txt rather than .html as extention
-#thus we do not need add <pre> into the files to make lines stand in different line
-    f.write('<a href="./html/')
-    f.write(s+".txt")
-    f.write('">')
+for s in listtt:
+    lists = s.split('/')
+    n = len(lists)
+    f.write(n*html_tab)
+    f.write('<a href="')
     f.write(s)
+    f.write('">')
+    ss=lists[n-1]
+    f.write(ss)
     f.write('</a><br>\n')
 f.close() #close index.html
-os.system("mv  *.txt ./peter-html/html/")
 
-ppp=os.listdir('./peter-html/html/')
-for ss in ppp:
-    print ss
-    #we want to add some tags into each file to make them pretty
