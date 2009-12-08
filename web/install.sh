bb4a476071ed2d799aa115982dbeebceab416f2c
happypeter <happypeter1983@gmail.com>
2009-12-08 00:37:48 -0700

    commit msg using github web interface

--- web/install.sh
+++ web/install.sh
@@ -2,3 +2,5 @@
 cp -rf cgi-bin/*  /usr/lib/cgi-bin/ #in ubuntu
 cp css/peter.css /var/www/css/ #depends on the path in header.html
 cp -rf muse/ ~/.emacs.d/ #depends on what in .emacs
+
+#doing edit with github web interface
