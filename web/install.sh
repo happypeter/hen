d946294ae3373b9439ed8840be753273ada1b4d7
pet <pet@cow.(none)>
2010-01-12 21:49:06 +0800

    useful change

--- web/install.sh
+++ web/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp -rf cgi-bin/*  /usr/lib/cgi-bin/ #in ubuntu
-cp css/peter.css /var/www/css/ #depends on the path in header.html
-cp -rf muse/ ~/.emacs.d/ #depends on what in .emacs
 
-#doing edit with github web interface
+sudo cp -rf cgi-bin/*  /usr/lib/cgi-bin/ #in ubuntu
+sudo cp css/peter.css /var/www/css/ #depends on the path in header.html
+sudo cp -rf muse/ ~/.emacs.d/ #depends on what in .emacs
