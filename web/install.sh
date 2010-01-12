162655f75e2db5e5f4065de31ca71753086c5616
pet <pet@cow.(none)>
2010-01-12 21:55:57 +0800

    d

--- web/install.sh
+++ web/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo cp -rf cgi-bin/*  /usr/lib/cgi-bin/ #in ubuntu
+sudo cp -rf cgi-bin/*  /usr/lib/cgi-bin/ 
+#this is the place to put cgi scripts in ubuntu
+
 sudo cp css/peter.css /var/www/css/ #depends on the path in header.html
+
 sudo cp -rf muse/ ~/.emacs.d/ #depends on what in .emacs
+
+sudo rm /var/www/*.html
+# now I can go to emacs and publish the new web with C-c-p
