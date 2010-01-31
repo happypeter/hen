a98b7e038baee8ecf717227ede8085117fe9995b
Peter Wang <happypeter1983@gmail.com>
2010-01-31 17:54:36 +0800

    port all files form the project php, and removed that project

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<?php
+ 
+print "hello";
+//<-- you need to put this file in www root, and view it by http://127.0.0.1 -->
+//<-- php code will be ignored, if you just double click it -->
+//<-- this is because php is activated as a mod of apache -->
+?>
+</body>
+</html>
+ 
