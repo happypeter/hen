126b224d7412919e23e3d5d4730ad3363525db5f
vivion-git <jz0923@yahoo.cn>
2010-05-08 14:16:33 +0800

    hello again
    http://www.pythonware.com/library/tkinter/introduction/hello-again.htm

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-# File: hello1.py
-
 from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
 
 root = Tk()
 
-w = Label(root, text="Hello, world!")
-w.pack()
+app = App(root)
 
 root.mainloop()
 
