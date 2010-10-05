136344b4ea6f8f72b1d1f1b7e69d9f04054730e4
Peter Wang <happypeter1983@gmail.com>
2010-10-05 18:05:32 +0800

    sizeof

--- C/exoweb.c
+++ C/exoweb.c
@@ -56,3 +56,40 @@ int find_largest(int b[])
     return k;
 
 }
+
+// and this post clarified this :
+// http://bytes.com/topic/c/answers/519368-how-know-size-integer-array
+//
+// ===POST=== 
+// In either of the prototypes
+//
+// int count(int thearray[])
+// int Max(int thearray[])
+//
+// thearray is declared as a pointer, you are probably getting confused
+// because you are using the [] notation but this does not declare an array
+// this is just another way of writting
+//
+// int Max(int *thearray)
+//
+// and a fairly confusing one as you have found out. sizeof(int *) == 4 on a
+// lot of systems (and so it seems on yours).
+//
+// You can not find the size of an array from a pointer to it, it is
+// impossible, you have to have access to the array it self to find it's size.
+// This means that if you are goinf to pass an array by passing a pointer to
+// it (a normal thing to do) then you need to pass the size of the array as
+// well. The one acception to this is char array where it is being use to
+// store a string because you can use the NULL terminator '\0' to locate the
+// end of the string (but not the size of the array).
+//
+// What you should do is change your prototype of Max to
+//
+// int Max(int *thearray, int number_of_ints)
+//
+// and in main (where thearray is defined) call Max as
+//
+// max = Max(thearray, (sizeof thearray)/(sizeof thearry[0]));
+//
+// This will work because you are using the sizeof opertor in the same context
+// that the array is declared in. 
