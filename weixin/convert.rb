a4772dea57c9d0114f5164076051a7f93ea32eac
Peter Wang <happypeter1983@gmail.com>
2015-05-07 11:26:59 +0800

    i

--- weixin/convert.rb
+++ weixin/convert.rb
@@ -12,30 +12,41 @@ num = str.scan(/\d{8}/).count
 
 count_string = "<div class='count'>" + num.to_s + "</div>" + "\n\n\n"
 
-front = <<HEREDOC
-hello peter
+header = <<HEREDOC
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>weixin</title>
+</head>
+<body>
 HEREDOC
 
+footer = <<HEREDOC
+
+</body>
+</html>
+HEREDOC
 
 # 每一个 div 以 时间戳为开始符，以空行为结束符，所以注意一个微信内部千万不要有空行
 
 str = str.gsub(/\d{8}/, "<div class='card' id='#{$&}'>")
           .gsub(/^\s*$/, "</div>\n")   # 可以包含0或多个空格的空行
 
-File.open("index.md", 'w') do |f|
-  f.write(front)
+File.open("index.html", 'w') do |f|
+  f.write(header)
 end
 
-
-
-File.open("index.md", 'a') do |f|
+File.open("index.html", 'a') do |f|
   f.write(count_string)
 end
 
-
-File.open("index.md", 'a') do |f|
+File.open("index.html", 'a') do |f|
   f.write(str + "</div>")   # 最后要封口
 end
 
+File.open("index.html", 'a') do |f|
+  f.write(footer)   # 最后要封口
+end
 
 # system 'ssh peter@happyec.org "source .profile &&/home/peter/bin/card_deploy.sh"'
